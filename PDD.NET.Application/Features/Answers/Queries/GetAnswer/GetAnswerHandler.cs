0d3e46cf8bdfa57bd5ac8d36b46c43e535b5997a
Роман Кириллов <RKirillov@live.ru>
2024-05-27 23:33:22 +0300

    feat: added AnswersController, need to test

---
+++ PDD.NET.Application/Features/Answers/Queries/GetAnswer/GetAnswerHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using PDD.NET.Application.Common.Exceptions;
+using PDD.NET.Application.Repositories;
+using PDD.NET.Domain.Entities;
+
+namespace PDD.NET.Application.Features.Answers.Queries.GetAnswer;
+
+public sealed class GetAnswerHandler : IRequestHandler<GetAnswerRequest, GetAnswerResponse>
+{
+    private readonly IAnswerRepository _answerRepository;
+    private readonly IMapper _mapper;
+
+    public GetAnswerHandler(IAnswerRepository answerRepository, IMapper mapper)
+    {
+        _answerRepository = answerRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<GetAnswerResponse> Handle(GetAnswerRequest request, CancellationToken cancellationToken)
+    {
+        var answer = _mapper.Map<User>(await _answerRepository.Get(request.Id, cancellationToken));
+        if (answer == null)
+        {
+            throw new NotFoundException(nameof(User), request.Id);
+        }
+
+        return _mapper.Map<GetAnswerResponse>(answer);
+    }
+}
---
+++ PDD.NET.Application/Features/Answers/Queries/GetAnswer/GetAnswerMapper.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using PDD.NET.Domain.Entities;
+
+namespace PDD.NET.Application.Features.Answers.Queries.GetAnswer;
+
+public sealed class GetAnswerMapper : Profile
+{
+    public GetAnswerMapper()
+    {
+        CreateMap<AnswerOption, GetAnswerResponse>();
+    }
+}
