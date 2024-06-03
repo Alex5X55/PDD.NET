436ffbb96509bda31ca5fb9291d4e0d059ee6823
Neverhood93 <Neverhood93@gmail.com>
2024-06-03 16:10:21 +0300

    В проект импортированы стили css от bootstrap. Изменена структура Header - добавлены компоненты из bootstrap. HomePage сделана в черновом варианте.

--- PDD.NET.Client/pdd-net-client/src/components/app-header/app-header.tsx
+++ PDD.NET.Client/pdd-net-client/src/components/app-header/app-header.tsx
@@ -1,7 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 export default function AppHeader() {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">PDD.NET</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="/" end>
+              Home
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
