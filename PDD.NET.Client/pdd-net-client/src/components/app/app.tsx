c3cdb965203a7c19e5ead3be11c2d27b56abbf38
Neverhood93 <Neverhood93@gmail.com>
2024-06-03 13:05:28 +0300

    Добавлен Header и HomePage.

---
+++ PDD.NET.Client/pdd-net-client/src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AppHeader from "../app-header/app-header";
+import HomePage from "../../pages/home/home-page";
+
+function App() {
+  return (
+    <>
+      <AppHeader />
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
