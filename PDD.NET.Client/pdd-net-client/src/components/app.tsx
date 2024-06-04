7ebb48d5f10c161b5fafce79bc03c93f9b4529dd
Neverhood93 <Neverhood93@gmail.com>
2024-06-04 09:35:44 +0300

    Добавлена болванка для страницы Входа.

--- PDD.NET.Client/pdd-net-client/src/components/app.tsx
+++ PDD.NET.Client/pdd-net-client/src/components/app.tsx
@@ -6,6 +6,7 @@ import NotFoundPage from "../pages/not-found-page";
 import RegisterPage from "../pages/register-page";
 import AppFooter from "./app-footer";
 import Container from "react-bootstrap/Container";
+import LoginPage from "../pages/login-page";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
 
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
