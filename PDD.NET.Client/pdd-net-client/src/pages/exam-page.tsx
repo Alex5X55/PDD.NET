21150d96760e1116d3f7e6652681216dfad2c003
Neverhood93 <Neverhood93@gmail.com>
2024-06-07 20:10:48 +0300

    Добавлены кнопки Начать экзамен и Завершить экзамен.

--- PDD.NET.Client/pdd-net-client/src/pages/exam-page.tsx
+++ PDD.NET.Client/pdd-net-client/src/pages/exam-page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import QuestionNumberList from "../components/question-number-list";
 import QuestionCard from "../components/question-card";
@@ -6,23 +6,48 @@ import Button from "react-bootstrap/Button";
 import useQuestionNavigation from "../hooks/use-question-navigation";
 
 const ExamPage: React.FC = () => {
+  const [isStart, setIsStart] = useState<boolean>(false);
   const { currentQuestions } = useQuestionNavigation(true);
+
+  const onStartHandleClick = () => {
+    setIsStart(true);
+  };
+
+  const onFinishHandleClick = () => {
+    setIsStart(false);
+  };
 
   return (
     <div className="container">
       <header className="jumbotron mt-5">
         <h1 className="display-4 mb-4">Экзамен</h1>
       </header>
-      <Button variant="primary" className="mb-3">
-        Начать экзамен
-      </Button>
-      <Button variant="primary" className="mb-3">
-        Завершить экзамен
-      </Button>
-      <QuestionNumberList questions={currentQuestions} />
-      <Routes>
-        <Route path=":questionId" element={<QuestionCard />} />
-      </Routes>
+      {!isStart ? (
+        <>
+          <Button
+            variant="primary"
+            className="mb-3"
+            onClick={onStartHandleClick}
+          >
+            Начать экзамен
+          </Button>
+          <div>После нажатия на кнопку "Начать экзамен", пойдет таймер.</div>
+        </>
+      ) : (
+        <>
+          <Button
+            variant="primary"
+            className="mb-3"
+            onClick={onFinishHandleClick}
+          >
+            Завершить экзамен
+          </Button>
+          <QuestionNumberList questions={currentQuestions} />
+          <Routes>
+            <Route path=":questionId" element={<QuestionCard />} />
+          </Routes>
+        </>
+      )}
     </div>
   );
 };
