e5f4aa2d70b22dceee3c5873f2f0a73c941b9c75
Neverhood93 <Neverhood93@gmail.com>
2024-06-07 20:21:38 +0300

    Добавлен таймер с логикой старта и завершения.

--- PDD.NET.Client/pdd-net-client/src/pages/exam-page.tsx
+++ PDD.NET.Client/pdd-net-client/src/pages/exam-page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import QuestionNumberList from "../components/question-number-list";
 import QuestionCard from "../components/question-card";
@@ -7,14 +7,39 @@ import useQuestionNavigation from "../hooks/use-question-navigation";
 
 const ExamPage: React.FC = () => {
   const [isStart, setIsStart] = useState<boolean>(false);
+  const [timeLeft, setTimeLeft] = useState<number>(1200); // 20 минут в секундах
   const { currentQuestions } = useQuestionNavigation(true);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (isStart && timeLeft > 0) {
+      timer = setInterval(() => {
+        setTimeLeft((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (timeLeft === 0) {
+      onFinishHandleClick();
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isStart, timeLeft]);
 
   const onStartHandleClick = () => {
     setIsStart(true);
+    setTimeLeft(1200);
   };
 
   const onFinishHandleClick = () => {
     setIsStart(false);
+    alert("Экзамен завершен!");
+  };
+
+  const formatTime = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
   };
 
   return (
@@ -35,13 +60,16 @@ const ExamPage: React.FC = () => {
         </>
       ) : (
         <>
-          <Button
-            variant="primary"
-            className="mb-3"
-            onClick={onFinishHandleClick}
-          >
-            Завершить экзамен
-          </Button>
+          <div className="d-flex justify-content-between align-items-center mb-3">
+            <Button variant="primary" onClick={onFinishHandleClick}>
+              Завершить экзамен
+            </Button>
+            <p className="fs-2 fw-bold me-4">
+              <div className="timer" style={{ marginLeft: "auto" }}>
+                {formatTime(timeLeft)}
+              </div>
+            </p>
+          </div>
           <QuestionNumberList questions={currentQuestions} />
           <Routes>
             <Route path=":questionId" element={<QuestionCard />} />
