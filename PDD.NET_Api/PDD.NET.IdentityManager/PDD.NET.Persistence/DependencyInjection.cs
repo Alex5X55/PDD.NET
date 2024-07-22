49dddbb1cdbd592e3930f50a1421c17bb7bbb9b4
Roman Kirillov <RKirillov@live.ru>
2024-07-22 12:24:11 +0300

    fix

--- PDD.NET_Api/PDD.NET.IdentityManager/PDD.NET.Persistence/DependencyInjection.cs
+++ PDD.NET_Api/PDD.NET.IdentityManager/PDD.NET.Persistence/DependencyInjection.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PDD.NET.Application.Repositories;
 using PDD.NET.Persistence.Repositories;
+using System.Text;
+using TodoAPI.Data;
 
 namespace PDD.NET.Persistence;
 
@@ -21,6 +23,39 @@ public static class DependencyInjection
         services.AddScoped<IUserInRoleRepository, UserInRoleRepository>();
 
         services.AddScoped<IDataInitializer, EFDataInitializer>();
+
+        services.AddDbContext<ApiDbContext>(opt =>
+    opt.UseSqlite(builder.Configuration.GetConnectionString("Default"))
+);
+
+        //JWT Config
+        services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+
+        // Validation params
+        Byte[]? key = Encoding.ASCII.GetBytes(builder.Configuration["JwtConfig:Secret"]);
+        TokenValidationParameters? tokenValidationParams = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            RequireExpirationTime = false
+        };
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(jwt =>
+        {
+
+            jwt.SaveToken = true;
+            jwt.TokenValidationParameters = tokenValidationParams;
+        });
+
+        services.AddSingleton(tokenValidationParams);
 
         return services;
     }
---
+++ PDD.NET_Api/PDD.NET.IdentityManager/PDD.NET.WebApi/Services/TokenManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDD.NET.WebApi.Services
+{
+    public static class TokenManager
+    {
+        public static string GenerateToken(string username)
+        {
+            /*        byte[] key = Convert.FromBase64String(Secret);
+                    SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+                    SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = new ClaimsIdentity(new[] {
+                                  new Claim(ClaimTypes.Name, username)}),
+                        Expires = DateTime.UtcNow.AddMinutes(30),
+                        SigningCredentials = new SigningCredentials(securityKey,
+                        SecurityAlgorithms.HmacSha256Signature)
+                    };
+
+                    JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+                    JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
+                    return handler.WriteToken(token);*/
+            return "";
+        }
+    }
+
+}
